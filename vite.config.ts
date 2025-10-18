c7e30911bc5215a27488226b8cfe290acf6d31ad
KTCpanda <ktc24a31a0006@edu.kyoto-tech.ac.jp>
2025-10-18 13:51:16 +0900

    ok

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-})
+export default defineConfig(({ command }) => ({
+  plugins: [react()],
+  base: command === 'serve' ? '/' : '/react-pokemon-zukan/',
+}));
+
+
